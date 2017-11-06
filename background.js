db3952249d2ba6fe6ae9f87a7c52f8142ce1d3eb
maksym-gryb <maksym.gryb@gmail.com>
2017-11-06 17:26:23 -0500

    Testing modification

---
+++ background.js
@@ -0,0 +1,29 @@
+/**
+ * Returns all of the registered extension commands for this extension
+ * and their shortcut (if active).
+ *
+ * Since there is only one registered command in this sample extension,
+ * the returned `commandsArray` will look like the following:
+ *    [{
+ *       name: "toggle-feature",
+ *       description: "Send a 'toggle-feature' event to the extension"
+ *       shortcut: "Ctrl+Shift+U"
+ *    }]
+ */
+var gettingAllCommands = browser.commands.getAll();
+gettingAllCommands.then((commands) => {
+    for (let command of commands) {
+        console.log(command);
+    }
+});
+
+/**
+ * Fired when a registered command is activated using a keyboard shortcut.
+ *
+ * In this sample extension, there is only one registered command:
+ * "Ctrl+Shift+U". On Mac, this command will automatically be converted to
+ * "Command+Shift+U".
+ */
+browser.commands.onCommand.addListener((command) => {
+    console.log('onCommand event received for message: ', command);
+});
