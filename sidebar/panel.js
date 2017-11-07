318a925d4b2818d86c3829f0324cf7de2cc2788b
maksym-gryb <maksym.gryb@gmail.com>
2017-11-06 20:08:35 -0500

    Testing

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,7 +1,19 @@
 var log_sidepanel = document.querySelector('#log');
-var x = 0;
 
-window.addEventListener('mouseover', () => {
-    x++;
-    log_sidepanel.innerHTML = x;
+var gettingAllCommands = browser.commands.getAll();
+gettingAllCommands.then((commands) => {
+    for (let command of commands) {
+        log_sidepanel.innerHTML = command;
+    }
+});
+
+/**
+ * Fired when a registered command is activated using a keyboard shortcut.
+ *
+ * In this sample extension, there is only one registered command:
+ * "Ctrl+Shift+U". On Mac, this command will automatically be converted to
+ * "Command+Shift+U".
+ */
+browser.commands.onCommand.addListener((command) => {
+    log_sidepanel.innerHTML = command;
 });
